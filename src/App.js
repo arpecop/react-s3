5a7debc43caa1d68a657929073d299e1d35e16e2
Rudi <rudix.lab@gmail.com>
2020-06-20 21:12:48 +0300

    c

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    Hello RudixWorld
                 </a>
             </header>
         </div>
