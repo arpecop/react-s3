2d6acd99279636471a20181bd0700f17526608fa
Rudi <rudix.lab@gmail.com>
2020-06-21 09:54:05 +0300

    c

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-function App() {
+export default function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>daadsads</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Hello RudixWorld
-                </a>
-            </header>
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/:id">
+                        <About />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
-export default App
+const About = (props) => {
+    return <div>dsad</div>
+}
